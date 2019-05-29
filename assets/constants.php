69254967deb07b6c123179456d216614da8d300e
jos <jos.mutter@hotmail.com>
2019-05-29 16:43:20 +0200

    Gave the constants a value

--- assets/constants.php
+++ assets/constants.php
@@ -1,11 +1,11 @@
 <?php
 // <editor-fold desc="Database type constants">
 const DATATYPE_VARCHAR = 0;
-const DATATYPE_INT = 0;
-const DATATYPE_DATE = 0;
-const DATATYPE_DATETIME = 0;
-const DATATYPE_TIME = 0;
-const DATATYPE_BOOLEAN = 0;
-const DATATYPE_DOUBLE = 0;
-const DATATYPE_FLOAT = 0;
+const DATATYPE_INT = 1;
+const DATATYPE_DATE = 2;
+const DATATYPE_DATETIME = 3;
+const DATATYPE_TIME = 4;
+const DATATYPE_BOOLEAN = 5;
+const DATATYPE_DOUBLE = 6;
+const DATATYPE_FLOAT = 7;
 // </editor-fold>
